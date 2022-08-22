b9b3a0b28ca9517a14bc632d52527bfabb5cec4b
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-08-22 12:37:32 -0300

    OX 7 Lección 12

--- facturacion/src/main/java/com/tuempresa/facturacion/modelo/DocumentoComercial.java
+++ facturacion/src/main/java/com/tuempresa/facturacion/modelo/DocumentoComercial.java
@@ -1,9 +1,11 @@
 package com.tuempresa.facturacion.modelo;
 
+import java.math.*;
 import java.time.*;
 import java.util.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 import org.openxava.annotations.*;
 import org.openxava.calculators.*;
@@ -43,10 +45,30 @@ abstract public class DocumentoComercial extends Identificable{
     Cliente cliente;
     
     @ElementCollection
-    @ListProperties("producto.numero, producto.descripcion, cantidad, precioPorUnidad, importe") // precioPorUnidad añadida
+    @ListProperties(
+            "producto.numero, producto.descripcion, cantidad, precioPorUnidad, " +
+            "importe+[" + 
+            	"documentoComercial.porcentajeIVA," +
+            	"documentoComercial.iva," +
+            	"documentoComercial.importeTotal" +
+            "]" 
+        )	
     Collection<Detalle> detalles;
     
     @TextArea
     String observaciones;
+    
+    @Digits(integer=2, fraction=0) // Para indicar su tamaño
+    BigDecimal porcentajeIVA;
+       
+    @ReadOnly
+    @Money
+    @Calculation("sum(detalles.importe) * porcentajeIVA / 100")
+    BigDecimal iva;
+
+    @ReadOnly
+    @Money
+    @Calculation("sum(detalles.importe) + iva")    
+    BigDecimal importeTotal;    
 
 }
