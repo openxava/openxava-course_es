08f78952d80496a832f74df35db0d208f8370651
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-08-24 11:22:41 -0300

    OX 7 Lección 16

--- facturacion/src/main/java/com/tuempresa/facturacion/modelo/Pedido.java
+++ facturacion/src/main/java/com/tuempresa/facturacion/modelo/Pedido.java
@@ -1,5 +1,7 @@
 package com.tuempresa.facturacion.modelo;
 
+import java.time.*;
+
 import javax.persistence.*;
 
 import org.openxava.annotations.*;
@@ -7,8 +9,10 @@ import org.openxava.annotations.*;
 import lombok.*;
 
 @Entity @Getter @Setter
-@View( extendsView="super.DEFAULT", // La vista por defecto
-members="factura { factura } "
+@View(extendsView="super.DEFAULT", 
+members=
+    "diasEntregaEstimados," + // AÑADE ESTA LÍNEA
+    "factura { factura }"
 )
 @View( name="SinClienteNiFactura", // Una vista llamada SinClienteNiFactura
 members=                       // que no incluye cliente ni factura
@@ -21,5 +25,23 @@ public class Pedido extends DocumentoComercial{
     @ManyToOne
     @ReferenceView("SinClienteNiPedidos") // Esta vista se usa para visualizar factura
     private Factura factura;
-	
+
+    @Depends("fecha")
+    public int getDiasEntregaEstimados() {
+        if (getFecha().getDayOfYear() < 15) {
+            return 20 - getFecha().getDayOfYear(); 
+        }
+        if (getFecha().getDayOfWeek() == DayOfWeek.SUNDAY) return 2;
+        if (getFecha().getDayOfWeek() == DayOfWeek.SATURDAY) return 3;
+        return 1;
+    }
+    
+    @Column(columnDefinition="INTEGER DEFAULT 1")
+    int diasEntrega;
+    
+    @PrePersist @PreUpdate 
+    private void recalcularDiasEntrega() {
+        setDiasEntrega(getDiasEntregaEstimados());
+    }
+    
 }
