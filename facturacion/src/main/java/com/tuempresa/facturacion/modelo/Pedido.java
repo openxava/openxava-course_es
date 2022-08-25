f20c2ab57a4be165a25e229183354144b21b351d
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-08-25 12:51:34 -0300

    OX 7 Lección 19

--- facturacion/src/main/java/com/tuempresa/facturacion/modelo/Pedido.java
+++ facturacion/src/main/java/com/tuempresa/facturacion/modelo/Pedido.java
@@ -20,18 +20,18 @@ members=                       // que no incluye cliente ni factura
     "detalles;" +
     "observaciones"
 )
+//Alternativa validacion por validator, en i18n debe ser 
+//pedido_debe_estar_entregado=Pedido {0}/{1} debe estar entregado para ser añadido a una Factura
 @EntityValidator(
-	    value=com.tuempresa.facturacion.validadores.ValidadorEntregadoParaEstarEnFactura.class, // Clase con la lógica de validación
-	    properties= {
-	        @PropertyValue(name="anyo"), // El contenido de estas propiedades
-	        @PropertyValue(name="numero"), // se mueve desde la entidad 'Pedido'
-	        @PropertyValue(name="factura"), // al validador antes de
-	        @PropertyValue(name="entregado") // ejecutar la validación
-	})
+value=com.tuempresa.facturacion.validadores.ValidadorEntregadoParaEstarEnFactura.class,
+properties= {
+  @PropertyValue(name="anyo"),
+  @PropertyValue(name="numero"),
+  @PropertyValue(name="factura"),
+  @PropertyValue(name="entregado")
+})
 public class Pedido extends DocumentoComercial{
 
-    @Column(columnDefinition="BOOLEAN DEFAULT FALSE")
-    boolean entregado;
     
     @ManyToOne
     @ReferenceView("SinClienteNiPedidos") // Esta vista se usa para visualizar factura
@@ -55,5 +55,44 @@ public class Pedido extends DocumentoComercial{
         setDiasEntrega(getDiasEntregaEstimados());
     }
     
+    @Column(columnDefinition="BOOLEAN DEFAULT FALSE")
+    boolean entregado;
+    
+//Alternativa de validación retrollamada JPA
+//    @PrePersist @PreUpdate // Antes de crear o modificar
+//    private void validar() throws Exception {
+//        if (factura != null && !isEntregado()) { // La lógica de validación
+//            // La excepción de validación del entorno Bean Validation
+//            throw new javax.validation.ValidationException(
+//                XavaResources.getString( // Para leer un mensaje i18n
+//                    "pedido_debe_estar_entregado",
+//                    getAnyo(),
+//                    getNumero())
+//            );
+//        }
+//    }
+    
+//Alternativa de validación setter
+//    public void setFactura(Factura factura) {
+//        if (factura != null && !isEntregado()) { // La lógica de validación
+//            // La excepción de validación del entorno Bean Validation
+//            throw new javax.validation.ValidationException(
+//                XavaResources.getString( // Para leer un mensaje i18n
+//                    "pedido_debe_estar_entregado",
+//                    getAnyo(),
+//                    getNumero())
+//            );
+//        }
+//        this.factura = factura; // La asignación típica del setter
+//    }
+    
+//Alternativa de validación Bean Validation, en i18n debe ser 
+//pedido_debe_estar_entregado=Pedido {anyo}/{numero} debe estar entregado para ser añadido a una Factura
+//    @AssertTrue(  // Antes de grabar confirma que el método devuelve true, si no lanza una excepción
+//    	    message="pedido_debe_estar_entregado" // Mensaje de error en caso retorne false
+//    	)
+//    	private boolean isEntregadoParaEstarEnFactura() { // ...
+//    	    return factura == null || isEntregado(); // La lógica de validación
+//    	}
     
 }
