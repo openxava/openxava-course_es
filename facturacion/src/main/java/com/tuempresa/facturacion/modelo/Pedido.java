b1af12f12bb95bd1b48bfe4e04c22f4fd422b4f7
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-08-25 14:05:06 -0300

    OX 7 Lección 20

--- facturacion/src/main/java/com/tuempresa/facturacion/modelo/Pedido.java
+++ facturacion/src/main/java/com/tuempresa/facturacion/modelo/Pedido.java
@@ -3,8 +3,10 @@ package com.tuempresa.facturacion.modelo;
 import java.time.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 import org.openxava.annotations.*;
+import org.openxava.util.*;
 
 import lombok.*;
 
@@ -20,18 +22,7 @@ members=                       // que no incluye cliente ni factura
     "detalles;" +
     "observaciones"
 )
-//Alternativa validacion por validator, en i18n debe ser 
-//pedido_debe_estar_entregado=Pedido {0}/{1} debe estar entregado para ser añadido a una Factura
-@EntityValidator(
-value=com.tuempresa.facturacion.validadores.ValidadorEntregadoParaEstarEnFactura.class,
-properties= {
-  @PropertyValue(name="anyo"),
-  @PropertyValue(name="numero"),
-  @PropertyValue(name="factura"),
-  @PropertyValue(name="entregado")
-})
 public class Pedido extends DocumentoComercial{
-
     
     @ManyToOne
     @ReferenceView("SinClienteNiPedidos") // Esta vista se usa para visualizar factura
@@ -58,41 +49,23 @@ public class Pedido extends DocumentoComercial{
     @Column(columnDefinition="BOOLEAN DEFAULT FALSE")
     boolean entregado;
     
-//Alternativa de validación retrollamada JPA
-//    @PrePersist @PreUpdate // Antes de crear o modificar
-//    private void validar() throws Exception {
-//        if (factura != null && !isEntregado()) { // La lógica de validación
-//            // La excepción de validación del entorno Bean Validation
-//            throw new javax.validation.ValidationException(
-//                XavaResources.getString( // Para leer un mensaje i18n
-//                    "pedido_debe_estar_entregado",
-//                    getAnyo(),
-//                    getNumero())
-//            );
-//        }
-//    }
+    //Alternativa de validación Bean Validation, en i18n debe ser 
+    //pedido_debe_estar_entregado=Pedido {anyo}/{numero} debe estar entregado para ser añadido a una Factura
+    @AssertTrue(  // Antes de grabar confirma que el método devuelve true, si no lanza una excepción
+    	    message="pedido_debe_estar_entregado" // Mensaje de error en caso retorne false
+    	)
+    	private boolean isEntregadoParaEstarEnFactura() { // ...
+    	    return factura == null || isEntregado(); // La lógica de validación
+    	}
     
-//Alternativa de validación setter
-//    public void setFactura(Factura factura) {
-//        if (factura != null && !isEntregado()) { // La lógica de validación
-//            // La excepción de validación del entorno Bean Validation
-//            throw new javax.validation.ValidationException(
-//                XavaResources.getString( // Para leer un mensaje i18n
-//                    "pedido_debe_estar_entregado",
-//                    getAnyo(),
-//                    getNumero())
-//            );
-//        }
-//        this.factura = factura; // La asignación típica del setter
-//    }
-    
-//Alternativa de validación Bean Validation, en i18n debe ser 
-//pedido_debe_estar_entregado=Pedido {anyo}/{numero} debe estar entregado para ser añadido a una Factura
-//    @AssertTrue(  // Antes de grabar confirma que el método devuelve true, si no lanza una excepción
-//    	    message="pedido_debe_estar_entregado" // Mensaje de error en caso retorne false
-//    	)
-//    	private boolean isEntregadoParaEstarEnFactura() { // ...
-//    	    return factura == null || isEntregado(); // La lógica de validación
-//    	}
+    //Alternativ a de validacion al borrar con retrollamada
+    @PreRemove
+    private void validarPreBorrar() {
+        if (factura != null) { // La lógica de validación
+            throw new javax.validation.ValidationException( // Lanza una excepción runtime
+                XavaResources.getString( // Para obtener un mensaje de texto
+                    "no_puede_borrar_pedido_con_factura"));
+        }
+    }
     
 }
