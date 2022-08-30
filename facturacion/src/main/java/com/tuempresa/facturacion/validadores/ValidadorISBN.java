57b8eba7a37178761a6c094a3d8d22b7adc08160
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-08-30 04:56:57 -0300

    OX 7 Lección 22

--- facturacion/src/main/java/com/tuempresa/facturacion/validadores/ValidadorISBN.java
+++ facturacion/src/main/java/com/tuempresa/facturacion/validadores/ValidadorISBN.java
@@ -1,24 +1,50 @@
-package com.tuempresa.facturacion.validadores; // En el paquete 'validadores'
- 
-import javax.validation.*;
+package com.tuempresa.facturacion.validadores; 
 
+import javax.validation.*;
+import javax.ws.rs.client.*; // Para usar JAX-RS
+
+import org.apache.commons.logging.*; // Para usar Log
 import org.openxava.util.*;
 
 import com.tuempresa.facturacion.anotaciones.*;
  
-public class ValidadorISBN implements ConstraintValidator<ISBN, Object> {
+public class ValidadorISBN
+    implements ConstraintValidator<ISBN, Object> {
+	
+    private static Log log = LogFactory.getLog(ValidadorISBN.class); // Instancia 'log'
  
     private static org.apache.commons.validator.routines.ISBNValidator
-        validador = // De 'Commons Validator'
+        validador = 
             new org.apache.commons.validator.routines.ISBNValidator();
  
     public void initialize(ISBN isbn) {
  
     }
-    
-    // Contiene la lógica de validación
-    public boolean isValid(Object valor, ConstraintValidatorContext contexto) { 
+ 
+    public boolean isValid(Object valor, ConstraintValidatorContext contexto) {
         if (Is.empty(valor)) return true;
-        return validador.isValid(valor.toString()); // Usa 'Commons Validator'
+        if (!validador.isValid(valor.toString())) return false;
+        return existeISBN(valor); // Aquí hacemos la llamada REST
     }
+    
+    private boolean existeISBN(Object isbn) {
+        try {
+            // Aquí usamos JAX-RS para llamar al servicio REST
+            String respuesta = ClientBuilder.newClient()
+                .target("http://openlibrary.org/") // El sitio
+                .path("/api/books") // La ruta del servicio
+                .queryParam("jscmd", "data") // Los parámetros
+                .queryParam("format", "json")
+                .queryParam("bibkeys", "ISBN:" + isbn) // El ISBN es un parámetro
+                .request()
+                .get(String.class); // Una cadena con el JSON
+            return !respuesta.equals("{}"); // ¿Está el JSON vacío? Suficiente para nuestro caso
+        }
+        catch (Exception ex) {
+            log.warn("Imposible conectar a openlibrary.org " +
+                "para validar el ISBN. Validación fallida", ex);
+            return false; // Si hay errores asumimos que la validación falla
+        }
+    }
+    
 }
