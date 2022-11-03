6c321d6f9fa56fa4b000d9e2fe9894955f81fb71
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-11-03 12:17:36 -0300

    OX 7 Lección 24 fixes

--- facturacion/src/main/java/com/tuempresa/facturacion/modelo/Pedido.java
+++ facturacion/src/main/java/com/tuempresa/facturacion/modelo/Pedido.java
@@ -22,7 +22,7 @@ members=                       // que no incluye cliente ni factura
     "detalles;" +
     "observaciones"
 )
-@Tab(baseCondition = "eliminado = false")
+@Tab(baseCondition = "${eliminado} = false")
 @Tab(name="Eliminado", baseCondition = "eliminado = true")
 public class Pedido extends DocumentoComercial{
     
